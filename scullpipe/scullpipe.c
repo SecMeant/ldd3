111c196602ac19bafafb698a6721f80ad922b9f2
holz <secmeant@gmail.com>
2019-11-11 01:49:34 +0100

    scullpipe work in progress

--- scullpipe/scullpipe.c
+++ scullpipe/scullpipe.c
@@ -6,16 +6,17 @@
 #include <linux/kdev_t.h>
 #include <linux/compiler_attributes.h>
 #include <linux/semaphore.h>
+#include <linux/uaccess.h>
 
 #define SCULLP_BUF_SIZE 512
 
 static dev_t scullpipe_major = 0;
 static dev_t scullpipe_minor = 0;
 
-int scullpipe_open (struct inode *, struct file *);
-int scullpipe_release (struct inode *, struct file *);
-int scullpipe_read (struct file *, char __user *, size_t, loff_t *);
-int scullpipe_write (struct file *, const char __user *, size_t, loff_t *);
+static int scullpipe_open (struct inode *, struct file *);
+static int scullpipe_release (struct inode *, struct file *);
+static ssize_t scullpipe_read (struct file *, char __user *, size_t, loff_t *);
+static ssize_t  scullpipe_write (struct file *, const char __user *, size_t, loff_t *);
 
 struct scullpipe_dev {
 	struct cdev cdev;
@@ -29,10 +30,12 @@ struct scullpipe_dev *sdev;
 static struct file_operations fops = {
 	.owner = THIS_MODULE,
 	.open = scullpipe_open,
-	.release = scullpipe_release
+	.release = scullpipe_release,
+	.read = scullpipe_read,
+	.write = scullpipe_write,
 };
 
-int scullpipe_open (struct inode *inode, struct file *filp)
+static int scullpipe_open (struct inode *inode, struct file *filp)
 {
 	filp->private_data = (void*) &sdev;
 	printk(KERN_DEBUG "Scullpipe open\n");
@@ -40,17 +43,17 @@ int scullpipe_open (struct inode *inode, struct file *filp)
 	return 0;
 }
 
-int scullpipe_release (struct inode *inode, struct file *filp)
+static int scullpipe_release (struct inode *inode, struct file *filp)
 {
 	printk(KERN_DEBUG "Scullpipe release");
 	return 0;
 }
 
-static __always_inline size_t readavail(const scullpipe_dev *sdev)
+static __always_inline size_t readavail(const struct scullpipe_dev *sdev)
 {
 	if (sdev->wp < sdev->rp)
 		return (sdev->bb - sdev->rp) + SCULLP_BUF_SIZE; // I hope this prevents overflow to happen
-	return sdev->wr - sdev->rp;
+	return sdev->wp - sdev->rp;
 }
 
 static __always_inline size_t scullmin(size_t a, size_t b)
@@ -59,7 +62,12 @@ static __always_inline size_t scullmin(size_t a, size_t b)
 	return a;
 }
 
-int scullpipe_read (struct file *filp, char __user *to, size_t count, loff_t off)
+static bool __always_inline cbempty(const struct scullpipe_dev *sdev)
+{
+	return sdev->rp == sdev->wp;
+}
+
+static ssize_t  scullpipe_read (struct file *filp, char __user *to, size_t count, loff_t *off)
 {
 	struct scullpipe_dev *sdev = (struct scullpipe_dev *) filp->private_data;
 
@@ -69,10 +77,10 @@ int scullpipe_read (struct file *filp, char __user *to, size_t count, loff_t off
 	while (sdev->rp == sdev->wp) {
 		up(&sdev->wsem);
 
-		if (sdev->f_flags & O_NONBLOCK)
+		if (filp->f_flags & O_NONBLOCK)
 			return -EAGAIN;
 
-		if (wait_event_interruptible(sdev->wsem, (sdev->rp != sdev->wp)))
+		if (wait_event_interruptible(sdev->rq, (!cbempty(sdev))))
 			return -ERESTARTSYS;
 
 		if (down_interruptible(&sdev->wsem))
@@ -81,9 +89,9 @@ int scullpipe_read (struct file *filp, char __user *to, size_t count, loff_t off
 
 	// There is data to read and semaphore is aquired
 	
-	count = scullmin(count, readavail());
+	count = scullmin(count, readavail(sdev));
 
-	if (copy_to_user(to, dev->rp, count)) {
+	if (copy_to_user(to, sdev->rp, count)) {
 		up (&sdev->wsem);
 		return -EFAULT;
 	}
@@ -96,6 +104,65 @@ int scullpipe_read (struct file *filp, char __user *to, size_t count, loff_t off
 	up(&sdev->wsem);
 
 	wake_up_interruptible(&sdev->wq);
+
+	return count;
+}
+
+static bool __always_inline cbfull(const struct scullpipe_dev *sdev)
+{
+	char * next = sdev->wp + 1;
+
+	if (next >= sdev->bb + SCULLP_BUF_SIZE)
+		next -= SCULLP_BUF_SIZE;
+
+	return next == sdev->rp;
+}
+
+static size_t __always_inline spacefree(const struct scullpipe_dev *sdev)
+{
+	if (sdev->rp > sdev->wp)
+		return sdev->rp - sdev->wp;
+
+	return (sdev->bb - sdev->wp) + SCULLP_BUF_SIZE;
+}
+
+static ssize_t scullpipe_write(struct file *filp, const char __user *from, size_t count, loff_t *off)
+{
+	struct scullpipe_dev *sdev = (struct scullpipe_dev *) filp->private_data;
+
+	if (down_interruptible(&sdev->wsem))
+		return -ERESTARTSYS;
+	
+	while (cbfull(sdev)) {
+		up(&sdev->wsem);
+
+		if (filp->f_flags & O_NONBLOCK)
+			return -EAGAIN;
+
+		if (wait_event_interruptible(sdev->wq, (!cbfull(sdev)) ))
+			return -ERESTARTSYS;
+
+		if (down_interruptible(&sdev->wsem))
+			return -ERESTARTSYS;
+	}
+
+	// There is space and semaphore is aquired
+	
+	count = scullmin(count, spacefree(sdev));
+
+	if (copy_from_user(sdev->wp, from, count)) {
+		up(&sdev->wsem);
+		return -EFAULT;
+	}
+
+	sdev->wp += count;
+
+	if (sdev->wp == sdev->bb + SCULLP_BUF_SIZE)
+		sdev->wp = sdev->bb;
+	
+	up(&sdev->wsem);
+
+	wake_up_interruptible(&sdev->rq);
 
 	return count;
 }
@@ -137,6 +204,9 @@ static int scullpipe_init(void)
 	sdev->wp = sdev->bb;
 	sdev->rp = sdev->bb;
 
+	init_waitqueue_head(&sdev->wq);
+	init_waitqueue_head(&sdev->rq);
+
 	cdev_init(&sdev->cdev, &fops);
 
 	ret = cdev_add(&sdev->cdev, dev, 1);
@@ -146,9 +216,6 @@ static int scullpipe_init(void)
 		kfree(sdev);
 		return ret;
 	}
-
-	init_waitqueuea_head(sdev->wq);
-	init_waitqueuea_head(sdev->rq);
 
 	return 0;
 }
